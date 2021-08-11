cb740ef765e2445a639d106361baefc9dc35fc50
Dave <60171703+meepdude121@users.noreply.github.com>
2021-08-11 15:26:30 +1200

    Updated gui graphics
    so much better jesus christ godd

---
+++ Assets/Pathfinding/Grid.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    public LayerMask unwalkableMask;
+    public Vector2 LevelSize;
+    public float nodeRadius;
+    public Node[,] grid;
+
+	private void OnDrawGizmos()
+	{
+		Gizmos.DrawWireCube(transform.position, new Vector3(LevelSize.x, LevelSize.y, 1));
+	}
+}
